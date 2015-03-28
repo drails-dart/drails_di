18ea0212fa220d00ecc04e586554f62fbf669aa9
luis <luisbigvargas@gmail.com>
2015-03-28 19:21:26 -0400

    * add documentation * pass component instead instanceMirror to pointCuts * modify After

--- lib/src/aop_proxy.dart
+++ lib/src/aop_proxy.dart
@@ -1,5 +1,45 @@
 part of drails_di;
 
+/// Class that must be extended in order to make the methods of an object interceptable
+/// 
+///     Class MyService {
+///       myMethod() => "inside myMethod";
+///     }
+///     
+///     Class MyServiceImpl extends MyService {
+///       myMethod() => super.myMethod() + "Impl";
+///       
+///       myMethodThrow() => throw new Exception("some message");
+///     }
+///     
+///     Class MyServiceProxy extends AopProxy implements MyService {}
+///     
+/// then in other file or in the same file you can create top level functions that are
+/// going to act as aspects/interceptors:
+/// 
+///     //Create the pointcut
+///     bool myService_myMethod(instance, Invocation invo) =>
+///       instance is MyService && invo.memberName == #myMethod;
+/// 
+///     //add the pointcut before
+///     @Before(myService_myMethod)
+///     printBeforeMyServiceMyMethod(Invocation invo) {
+///       print("before MyService.myMethod");
+///       //modify invo as needed
+///       return invo;
+///     }
+///     
+///     myService_myMethod_retVal(instance, Invocation invo, retVal) =>
+///       retVal == "myMethodImpl" && myService_myMethod();
+///     
+///     //add pointcut after
+///     @After(myService_myMethod_retVal)
+///     printAfterMyServiceMyMethod(retVal) {
+///       print(after MyService.myMethod");
+///       //modify retVal as needed
+///       return retVal;
+///     }
+///  
 class AopProxy {
   noSuchMethod(Invocation invocation) {
     var component = ApplicationContext._componentOfProxy[reflect(this).type.reflectedType];
@@ -8,7 +48,7 @@ class AopProxy {
     
     //Befor aspect begin
     ApplicationContext._aspectsBefore.keys.where((aspect) =>
-        new GetValueOfAnnotation<Before>().fromDeclaration(aspect).isBefore(im, invocation)
+        new GetValueOfAnnotation<Before>().fromDeclaration(aspect).isBefore(component, invocation)
     ).forEach((mm) {
       ApplicationContext._aspectsBefore[mm].invoke(mm.simpleName, []);
     });
@@ -27,15 +67,9 @@ class AopProxy {
       //Invoque method end
       
       //After aspect begin
-      ApplicationContext._aspectsAfter.keys.where((aspect) {
-        var afterAnn = new GetValueOfAnnotation<After>().fromDeclaration(aspect);
-        
-        if(!afterAnn.retValNull && afterAnn.retVal == retVal
-            || afterAnn.retValNull == (retVal == null))
-          return afterAnn.isAfter(im, invocation);
-        
-        return false;
-      }).forEach((mm) {
+      ApplicationContext._aspectsAfter.keys.where((aspect) =>
+        new GetValueOfAnnotation<After>().fromDeclaration(aspect).isAfter(component, invocation, retVal)
+      ).forEach((mm) {
         retVal = ApplicationContext._aspectsAfter[mm].invoke(mm.simpleName, [retVal]).reflectee;
       });
       //After aspect end
@@ -43,15 +77,15 @@ class AopProxy {
     } catch(ex, stack) {
       //AfterThrowing begin
       ApplicationContext._aspectsAfterThrowing.keys.where((aspect)  =>
-          new GetValueOfAnnotation<AfterThrowing>().fromDeclaration(aspect).isAfterThrowing(im, invocation, ex)
+          new GetValueOfAnnotation<AfterThrowing>().fromDeclaration(aspect).isAfterThrowing(component, invocation, ex)
       ).forEach((mm) {
-          retVal = ApplicationContext._aspectsAfterThrowing[mm].invoke(mm.simpleName, [ex]).reflectee;
+          retVal = ApplicationContext._aspectsAfterThrowing[mm].invoke(mm.simpleName, [retVal, ex]).reflectee;
       });
       //AfterThrowing end
     } finally {
       //AfterFinally begin
       ApplicationContext._aspectsAfterFinally.keys.where((aspect) =>
-        new GetValueOfAnnotation<AfterFinally>().fromDeclaration(aspect).pointCut(im, invocation)
+        new GetValueOfAnnotation<AfterFinally>().fromDeclaration(aspect).pointCut(component, invocation)
       ).forEach((mm) {
         retVal = ApplicationContext._aspectsAfterFinally[mm].invoke(mm.simpleName, [retVal]).reflectee;
       });
